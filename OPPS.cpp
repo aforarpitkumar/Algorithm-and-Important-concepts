3e00e928f6b733b1644be660e70753861732babd
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2022-01-18 23:54:24 +0530

    Create OPPS.cpp

---
+++ OPPS.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee{
+    public:
+    string Name;
+    string Company;
+    int Age;
+    
+    void Intro(){
+        
+        cout<<"Name - "<< Name<< endl;
+        cout<<"Company - "<< Company<< endl;
+        cout<<"Age - "<< Age<< endl;
+        
+    }
+    
+};
+
+
+int main()
+{
+    //cout<<"Hello World";
+    
+    Employee employee;
+    
+    employee.Name = "Arpit";
+    employee.Company = "HCL";
+    employee.Age = 23;
+    employee.Intro();
+
+    return 0;
+}
